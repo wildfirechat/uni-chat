0a0411022c85ee16827eb8ae9c2d516b928f6e9e
imndx <imndxx@gmail.com>
2022-06-30 23:17:42 +0800

    添加 client 原生插件，方便开发调试

---
+++ nativeplugins/wf-uni-wfc-client/README.md
@@ -0,0 +1,7 @@
+# 野火IM插件使用说明
+
+1. 参考```uni-chat```，将```wfc```目录和```config.js```文件拷贝到项目根目录
+2. 修改```config.js``` 里面的```APP_SERVER```和```IM_SERVER_HOST```
+3. 参考```uni-chat``` 实现登录和连接
+4. 支持接口及使用方法和```pc/web```一致，具体可以参考```wfc/client/wfc.js```文件
+5. 遇到问题，请到[uni-wfc-client](https://gitee.com/wfchat/uni-wfc-client) 项目下提issue，我们会尽快解决
---
+++ nativeplugins/wf-uni-wfc-client/ios/WFChatClient.framework/Headers/WFCCGroupSearchInfo.h
@@ -0,0 +1,38 @@
+//
+//  WFCCGroupSearchInfo.h
+//  WFChatClient
+//
+//  Created by heavyrain on 2017/10/22.
+//  Copyright © 2017年 WildFireChat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "WFCCGroupInfo.h"
+#import "WFCCJsonSerializer.h"
+
+/**
+ 群组搜索信息
+ */
+@interface WFCCGroupSearchInfo : WFCCJsonSerializer
+
+/**
+ 命中的群组
+ */
+@property (nonatomic, strong)WFCCGroupInfo *groupInfo;
+
+/**
+ 命中的类型 0, 名字群组名字； 1，命中群成员名称；2，都命中
+ */
+@property (nonatomic, assign)int marchType;
+
+/**
+ 命中群成员名称
+ */
+@property (nonatomic, strong)NSArray *marchedMemberNames;
+
+/**
+ 搜索的关键字
+ */
+@property (nonatomic, strong)NSString *keyword;
+
+@end
